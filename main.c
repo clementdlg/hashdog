d8288f388f8fc1ea50350fa764b9b35eb80b8c97
Clément DE LA GENIERE <0xkrem@proton.me>
2024-11-17 17:53:44 +0100

    switched to using arguments

--- main.c
+++ main.c
@@ -9,12 +9,16 @@ int main(int argc, char** argv) {
 	unsigned short bSize = 255;
 	char buffer[bSize];
 
-	printf("This program performs dictionnary attack on a hashed SHA-256 password\n");
-	printf("Insert path of dictionnary txt file : ");
-	scanf("%254s", dict);
-
-	printf("Insert hashed password : ");
-	scanf("%254s", target);
+	if (argc != 3) {
+		printf("This program performs dictionnary attack on a hashed SHA-256 password\n");
+		printf("It takes two arguments :\n");
+		printf("1 - Dictionnary file path\n");
+		printf("2 - SHA-256-hashed password\n");
+		exit(0);
+	}
+	// store arguments
+	strcpy(dict, argv[1]);
+	strcpy(target, argv[2]);
 
 	// setting a ptr to the data
 	FILE *pDict = fopen(dict, "r");
@@ -26,6 +30,7 @@ int main(int argc, char** argv) {
 	}
 
 	while (fgets(buffer, bSize, pDict) != NULL) {
+		//remove '\n' that prevent matching
 		buffer[strlen(buffer) - 1] = '\0';
 
 		if (strcmp(buffer, target) == 0) {
