65cb96e81b17df0795e0a8dd01d43d021491dca2
Clément DE LA GENIERE <0xkrem@proton.me>
2024-11-17 19:19:01 +0100

    proto 1 working

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/sha.h>
+
+int sha_hash(char *str, char *hash);
 
 int main(int argc, char** argv) {
 	char dict[255];
@@ -33,10 +36,29 @@ int main(int argc, char** argv) {
 		//remove '\n' that prevent matching
 		buffer[strlen(buffer) - 1] = '\0';
 
-		if (strcmp(buffer, target) == 0) {
+		char* hash = malloc(SHA256_DIGEST_LENGTH * 2 + 1);
+		if (hash == NULL) {
+			printf("Error: Memory allocation failed\n");
+		}
+
+		sha_hash(buffer, hash);
+
+		if (strcasecmp(hash, target) == 0) {
 			printf("%s\n", buffer);
 		}
 	}
 	
 	fclose(pDict);
+}
+
+int sha_hash(char *str, char *hash) {
+    unsigned char buff[SHA256_DIGEST_LENGTH]; 
+
+    SHA256((unsigned char *)str, strlen(str), buff);
+
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        sprintf(hash + (i * 2), "%02x", buff[i]);
+    }
+    hash[SHA256_DIGEST_LENGTH * 2] = '\0';
+    return 0;
 }
