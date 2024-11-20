e2c554eb3f814c687e66eedd441a1f6e815cc472
haddad thinina <thininathinina8@gmail.com>
2024-11-20 09:22:56 +0100

    header file md5

---
+++ md5.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdint.h>
+
+typedef struct {
+	uint32_t a, b, c, d;
+} md5_context;
+
+void md5_init(md5_context* ctx);
+void md5_digest(md5_context* ctx, void* buffer, size_t size);
+void md5_output(md5_context* ctx, uint8_t out[16]);
