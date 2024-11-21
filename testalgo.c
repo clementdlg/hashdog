d2d2aa25bc8ef007b4364f048d1e1e6f4669ee0f
haddad thinina <thininathinina8@gmail.com>
2024-11-21 19:30:34 +0100

    test file of bruteforce algo

---
+++ testalgo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bruteforce.h"
+
+
+
+int main() {
+  
+    char format[] = "isun";
+    char target[] = "b@Z9";
+
+    bruteForce(format, target);
+
+    return 0;
+}
